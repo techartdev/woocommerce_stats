e1a3b6c3dd468a3a352931529e35dfdb6173cfad
techartdev <admin@techart.bg>
2024-11-30 17:23:44 +0200

    test different initi

--- custom_components/woocommerce_stats/__init__.py
+++ custom_components/woocommerce_stats/__init__.py
@@ -25,17 +25,15 @@ async def async_setup(hass, config):
 
 async def async_setup_entry(hass, config_entry):
     """Set up WooCommerce Stats from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][config_entry.entry_id] = {
-        "url": config_entry.data["url"],
-        "consumer_key": config_entry.data["consumer_key"],
-        "consumer_secret": config_entry.data["consumer_secret"],
-    }
+    _LOGGER.debug("async_setup_entry called for WooCommerce Stats.")
 
-    _LOGGER.info("WooCommerce Stats integration has been set up via config entry.")
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
     return True
 
 async def async_unload_entry(hass, config_entry):
     """Unload a config entry."""
-    hass.data[DOMAIN].pop(config_entry.entry_id)
+    _LOGGER.debug("async_unload_entry called for WooCommerce Stats.")
+    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
     return True
