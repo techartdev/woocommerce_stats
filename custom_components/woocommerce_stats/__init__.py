6d24e1d6128ca065bc95ab9c8c9a3755eb47a501
techartdev <admin@techart.bg>
2024-11-30 15:45:44 +0200

    trying to fix setup

--- custom_components/woocommerce_stats/__init__.py
+++ custom_components/woocommerce_stats/__init__.py
@@ -2,6 +2,8 @@ from homeassistant.helpers import config_validation as cv
 import voluptuous as vol
 import logging
 
+_LOGGER = logging.getLogger(__name__)
+
 DOMAIN = "woocommerce_stats"
 
 CONFIG_SCHEMA = vol.Schema(
@@ -16,3 +18,24 @@ CONFIG_SCHEMA = vol.Schema(
     },
     extra=vol.ALLOW_EXTRA,
 )
+
+async def async_setup(hass, config):
+    """Set up WooCommerce Stats integration."""
+    return True
+
+async def async_setup_entry(hass, config_entry):
+    """Set up WooCommerce Stats from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = {
+        "url": config_entry.data["url"],
+        "consumer_key": config_entry.data["consumer_key"],
+        "consumer_secret": config_entry.data["consumer_secret"],
+    }
+
+    _LOGGER.info("WooCommerce Stats integration has been set up via config entry.")
+    return True
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(config_entry.entry_id)
+    return True
